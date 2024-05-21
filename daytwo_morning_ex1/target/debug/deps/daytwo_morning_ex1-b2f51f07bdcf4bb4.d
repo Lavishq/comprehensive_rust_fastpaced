cd9bb76605d02d6482cdf0719d9a96a81701999e
lavishq <96654752+Lavishq@users.noreply.github.com>
2024-05-21 15:07:34 +0200

    oof2

--- daytwo_morning_ex1/target/debug/deps/daytwo_morning_ex1-b2f51f07bdcf4bb4.d
+++
@@ -1,5 +0,0 @@
-/home/lavishq/comprehensive_rust_fastpaced/daytwo_morning_ex1/target/debug/deps/daytwo_morning_ex1-b2f51f07bdcf4bb4: src/main.rs
-
-/home/lavishq/comprehensive_rust_fastpaced/daytwo_morning_ex1/target/debug/deps/daytwo_morning_ex1-b2f51f07bdcf4bb4.d: src/main.rs
-
-src/main.rs:
